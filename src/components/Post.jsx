a39e416e86806c6ed00420967516a69e9752935a
sakir tufan <sakirtufan49@gmail.com>
2021-01-01 12:20:56 +0100

    show available comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 const Post = (props) => {
   const { id } = props.match.params;
   const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     axios.get(`https://react-yazi-yorum.herokuapp.com/posts/${id}`)
@@ -12,15 +13,37 @@ const Post = (props) => {
       })
       .catch((error) => {
         console.log(error);
+      });
+
+    axios.get(`https://react-yazi-yorum.herokuapp.com/posts/${id}/comments`)
+      .then((response) => {
+        setComments(response.data);
       })
       
   },[]);
 
   return (
     <React.Fragment>
-      <h2 className="ui header">{post.title}</h2>
+      <h2 classNameName="ui header">{post.title}</h2>
       <p>{post.created_at}</p>
       <p>{post.content}</p>
+{/*       Yorumlar
+      yorumlar Listesia
+      yorum yazma formu */}
+      {comments.map(comment => {
+        return(
+
+      <div key={comment.id} className="ui relaxed list">
+        <div className="item">
+          <img className="ui avatar image" src="/images/avatar/small/daniel.jpg"/>
+          <div className="content">
+            <a className="header">{comment.display_name}</a>
+            <div className="description">{comment.body}</div>
+          </div>
+        </div>
+      </div>
+        )
+      })}
     </React.Fragment>
   );
 };
